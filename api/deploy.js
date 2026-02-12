69d3db672d0af0fdb1adef9f6bb002339b009e80
CINEMAX-MV <cinemax544@gmail.com>
2026-02-12 19:30:05 +0530

    Update deploy.js

--- api/deploy.js
+++ api/deploy.js
@@ -1,1 +1,7 @@
 
+import { startSession } from "../../utils/session";
+
+export default function handler(req, res) {
+  startSession();
+  res.status(200).json({ message: "Bot deployed ✅ Running for 45 min..." });
+}
