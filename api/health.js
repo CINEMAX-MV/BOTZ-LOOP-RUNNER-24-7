e8d519cc4873bd2a58c41a1c2e59f775a9a40a9b
CINEMAX-MV <cinemax544@gmail.com>
2026-02-12 19:31:48 +0530

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,1 +1,8 @@
 
+import { restartSession } from "../../utils/session";
+
+export default function handler(req, res) {
+  // Self-ping keeps the bot “alive”
+  restartSession();
+  res.status(200).json({ health: "Server pinged 🔥" });
+}
