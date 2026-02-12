52f15ab87bda67a798bb6b5ab1673f4afec9a973
CINEMAX-MV <cinemax544@gmail.com>
2026-02-12 19:31:13 +0530

    Update restart.js

--- api/restart.js
+++ api/restart.js
@@ -1,1 +1,7 @@
 
+import { restartSession } from "../../utils/session";
+
+export default function handler(req, res) {
+  restartSession();
+  res.status(200).json({ message: "Bot restarted 🔄" });
+}
